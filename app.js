2c73594a663a087b6e978153532a31f59604e11d
Fanny <fannybecerravera@gmail.com>
2018-06-09 14:29:54 -0400

    Primer commit subiendo primer ejercicio

---
+++ app.js
@@ -0,0 +1,10 @@
+/* Escriba una función 'computeAverageLengthOfWords' 
+que recibe dos strings y devuelve el promedio de la longitud 
+de ambos strings. */ 
+
+  const computeAverageLengthOfWord = ((word1, word2)=>{
+return (word1.length + word2.length)/2
+  });
+  console.log(computeAverageLengthOfWords('hola', 'chao'));
+
+
