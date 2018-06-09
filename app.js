0097377b2c7ee9526e8182ca8a3194aab74b9ebe
Fanny <fannybecerravera@gmail.com>
2018-06-09 15:18:27 -0400

    Subiendo ejercicio tres

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ que recibe dos strings y devuelve el promedio de la longitud
 de ambos strings. */ 
 
   //arrow function
+
 const computeAverageLengthOfWord = ((word1, word2) => {
 return (word1.length + word2.length)/2;
   });
@@ -24,6 +25,7 @@ function getNthElement(array, n) {
   console.log(getNthElement([1, 3, 5, 7], 2));
 
   //arow function
+
 const getNthElement = ((array, n) => {
   for (let i = 0; i < array.length; i++){
     if (array.indexOf(n) === i);
@@ -31,3 +33,24 @@ const getNthElement = ((array, n) => {
     }
 });
 console.log(getNthElement([1, 3, 5, 7], 2));
+
+/* Ejercicio 3
+Escribe una función llamada convertDoubleSpaceToSingle.
+Dada una cadena, convertDoubleSpaceToSingle devuelve la 
+cadena pasada en cadena con todos los espacios dobles 
+convertidos a espacios simples. */
+
+function convertDoubleSpaceToSingle(str) {
+  let arrayStr = str.split("  ").join(" ");
+	return arrayStr;
+};
+console.log(convertDoubleSpaceToSingle("string  with  double  spaces"));
+
+//arrow function
+
+const convertDoubleSpaceToSingle = (str) => {
+	let arrayStr = str.split("  ").join(" ");
+	return arrayStr;
+};
+console.log(convertDoubleSpaceToSingle("string  with  double  spaces"));
+
